2551fbbddfd35db7947fd6fd0a52ffde8b5681c3
Garry Xiao <xz@etsoo.com>
2022-12-16 11:42:14 +1300

    Add project files.

---
+++ com.etsoo.Logs/Attributes/PIIAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace com.etsoo.Logs.Attributes
+{
+    /// <summary>
+    /// PII (Personally Identifiable Information) attribute
+    /// PII（个人身份信息）属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class PIIAttribute : Attribute
+    {
+    }
+}
---
+++ com.etsoo.Logs/Policies/PIIAttributeMaskingDestructuringPolicy.cs
@@ -0,0 +1,29 @@
+﻿using com.etsoo.Logs.Attributes;
+using Serilog.Core;
+using Serilog.Events;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace com.etsoo.Logs.Policies
+{
+    /// <summary>
+    /// PII (Personally Identifiable Information) destructuring policy, support PIIAttribute and JsonIgnoreAttribute
+    /// PII（个人身份信息）解构政策，支持 PIIAttribute 和 JsonIgnoreAttribute
+    /// </summary>
+    public class PIIAttributeMaskingDestructuringPolicy : IDestructuringPolicy
+    {
+        public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, [NotNullWhen(true)] out LogEventPropertyValue? result)
+        {
+            var type = value.GetType();
+            if (type.GetCustomAttribute<PIIAttribute>() != null || type.GetCustomAttribute<JsonIgnoreAttribute>() != null)
+            {
+                result = new ScalarValue("***");
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
